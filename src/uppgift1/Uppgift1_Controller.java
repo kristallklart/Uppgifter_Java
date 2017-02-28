32ab4ac68f3bada3c5013d08d71f6ac6aab90668
Jakob Lindh <hammerslay@gmail.com>
2017-02-28 15:51:17 +0100

    Färdigställde uppgift 1.

---
+++ src/uppgift1/Uppgift1_Controller.java
@@ -0,0 +1,11 @@
+package uppgift1;
+//import Uppgift1_OpenFile;
+
+import java.rmi.RemoteException;
+
+public class Uppgift1_Controller {
+	
+	public static String getFileContent(String s) throws RemoteException{
+		return Uppgift1_FileHandler.getFileContent(s);
+	}
+}
--- src/uppgift1/Uppgift1_View.java
+++ src/uppgift1/Uppgift1_View.java
@@ -1,12 +1,30 @@
 package uppgift1;
 
 import java.awt.EventQueue;
+import java.awt.ScrollPane;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.rmi.RemoteException;
 
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.JTextPane;
+import javax.swing.UIManager;
+import javax.swing.UIManager.LookAndFeelInfo;
+import javax.swing.border.TitledBorder;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 public class Uppgift1_View {
 
-	private JFrame frame;
+	private JFrame frameOpenFile;
+	private JFileChooser fileChooser;
+	private FileNameExtensionFilter filter; 
+	//Uppgift1_ServiceSoapProxy proxy = new Uppgift1_ServiceSoapProxy();
 
 	/**
 	 * Launch the application.
@@ -16,7 +34,7 @@ public class Uppgift1_View {
 			public void run() {
 				try {
 					Uppgift1_View window = new Uppgift1_View();
-					window.frame.setVisible(true);
+					window.frameOpenFile.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -28,6 +46,19 @@ public class Uppgift1_View {
 	 * Create the application.
 	 */
 	public Uppgift1_View() {
+		try{
+		for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+			if ("Windows".equals(info.getName())) {
+				UIManager.setLookAndFeel(info.getClassName());
+			}
+		}
+		} catch (Exception e) {
+			//Hantera fel här
+			e.printStackTrace();
+		}
+		fileChooser = new JFileChooser();
+    	filter = new FileNameExtensionFilter(".txt", "txt");
+    	fileChooser.setFileFilter(filter);
 		initialize();
 	}
 
@@ -35,9 +66,64 @@ public class Uppgift1_View {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frameOpenFile = new JFrame();
+		frameOpenFile.setTitle("Open File Application");
+		frameOpenFile.setBounds(100, 100, 270, 280);
+		frameOpenFile.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frameOpenFile.getContentPane().setLayout(null);
+		
+		JLabel lblFileName = new JLabel("File name");
+		lblFileName.setBounds(21, 63, 46, 14);
+		frameOpenFile.getContentPane().add(lblFileName);
+		
+		JLabel lblFileContent = new JLabel("File content");
+		lblFileContent.setBounds(21, 86, 65, 14);
+		frameOpenFile.getContentPane().add(lblFileContent);
+		
+		JTextField textFieldFileName;
+		textFieldFileName = new JTextField();
+		textFieldFileName.setEditable(false);
+		textFieldFileName.setBounds(80, 60, 155, 20);
+		frameOpenFile.getContentPane().add(textFieldFileName);
+		textFieldFileName.setColumns(10);
+		
+		JLabel lblFeedback = new JLabel("Message:");
+		lblFeedback.setBounds(21, 222, 210, 14);
+		frameOpenFile.getContentPane().add(lblFeedback);
+		
+		JTextPane textPaneContent = new JTextPane();
+		textPaneContent.setBounds(20, 105, 200, 100);
+		textPaneContent.setEditable(false);
+		
+		ScrollPane scrollPane = new ScrollPane();
+		scrollPane.setBounds(20, 105, 212, 102);
+		scrollPane.add(textPaneContent);
+		frameOpenFile.getContentPane().add(scrollPane);
+		
+		JPanel panelResult = new JPanel();
+		panelResult.setBorder(new TitledBorder(null, "Result", TitledBorder.LEFT, TitledBorder.TOP, null, null));
+		panelResult.setBounds(10, 40, 234, 180);
+		frameOpenFile.getContentPane().add(panelResult);
+		
+		// Exempel för att öppna filer: http://docs.oracle.com/javase/tutorial/uiswing/components/filechooser.html
+		JButton btnBrowseFile = new JButton("Browse file...");
+		btnBrowseFile.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				int returnVal = fileChooser.showOpenDialog(frameOpenFile); //alternativt null som parameter
+		        if (returnVal == JFileChooser.APPROVE_OPTION) {
+		        	File file = fileChooser.getSelectedFile();
+		            String fileName;
+		            fileName = file.getPath();
+		            try {
+		            	textPaneContent.setText(Uppgift1_Controller.getFileContent(fileName));
+						textFieldFileName.setText(file.getName());
+					} catch (RemoteException e) {
+						lblFeedback.setText("Message: Error, could not open file!");
+					} 
+		        }
+			}
+		});
+		btnBrowseFile.setBounds(10, 11, 97, 23);
+		frameOpenFile.getContentPane().add(btnBrowseFile);
 	}
-
 }
